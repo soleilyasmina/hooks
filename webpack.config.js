500eae5184edd1292d43320efc52afee359184ab
soleilyasmina <mounlasolomon@gmail.com>
2021-07-14 02:17:27 -0400

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,9 @@ const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  experiments: {
+    outputModule: true,
+  },
   externals: {
     react: "commonjs react",
     "react-dom": "commonjs react-dom",
